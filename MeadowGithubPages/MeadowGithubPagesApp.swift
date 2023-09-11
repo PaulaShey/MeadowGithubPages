0665cf2fc53cafc2b034f978822d1ce5ab2fc65e
Paulina Gruzewska <paula@wildland.io>
2023-09-11 14:55:31 +0200

    Add xcode project

---
+++ MeadowGithubPages/MeadowGithubPagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeadowGithubPagesApp.swift
+//  MeadowGithubPages
+//
+//  Created by Paulina Grużewska on 11/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct MeadowGithubPagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
